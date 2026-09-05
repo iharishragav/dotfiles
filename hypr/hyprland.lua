42514f1659d9655a2afb66b2d50ad0d9cbac78e4
kamalesh-sudo <ragavhrxh@gmail.com>
2026-09-05 20:14:16 +0530

    better wiring and optimal setup

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -484,7 +484,7 @@ end)   -- Special workspaces
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 hl.on("hyprland.start", function()
-hl.exec_cmd("~/.config/quickshell/launch.sh")
+hl.exec_cmd("~/.config/quickshell/launch.fish")
 end)
 
 
